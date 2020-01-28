405edd4fce62cacfe3526e25719c5f6632e803ab
robinsingh1011 <60376642+robinsingh1011@users.noreply.github.com>
2020-01-28 15:16:00 +0530

    Update Testrunner.java

--- src/main/java/com/acc/cucumber_27_jan/Testrunner.java
+++ src/main/java/com/acc/cucumber_27_jan/Testrunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin ={"json:target/cucumber.json:}
+@CucumberOptions(plugin ={"json:target/cucumber.json"}
 	features="Feature"
                  ,glue= {"step_definition"}
                  )
